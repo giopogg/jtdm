d26a3270f01c28497ce39e62139e0d0318e7ae2d
giopogg <49613661+giopogg@users.noreply.github.com>
2022-10-20 21:18:59 +0200

    add global name and .md to main

--- jtdm/R/global.R
+++ jtdm/R/global.R
@@ -1,8 +1,8 @@
 #' Global
 #' 
 #' Declare global variables
+#' @name global
 #' @importFrom utils globalVariables
 NULL
 
-## quiets concerns of R CMD check re: the .'s that appear in pipelines
-if(getRversion() >= "2.15.1")  utils::globalVariables(c("x", "Predmean", "Pred025", "Pred975"))
+if(getRversion() >= "2.15.1")  utils::globalVariables(c("x", "Predmean", "Pred025", "Pred975"), add = F)
